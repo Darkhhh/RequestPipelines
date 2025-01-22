ca625dac5de8b355ab0017412c403b3680ddc155
Darkhhh <a.a.mikov@mail.ru>
2025-01-22 13:26:24 +0300

    (dev): HANDP-10 - обновлены примеры использования синхронного конвейера.

---
+++ src/RequestPipelines/Runner/Example/StandalonePipelineExample.cs
@@ -0,0 +1,26 @@
+﻿using RequestPipelines.PipelineBuilder;
+using Runner.Example.Handlers;
+using Runner.Example.Models;
+
+namespace Runner.Example;
+
+public class StandalonePipelineExample
+{
+    public static void Example()
+    {
+        var initModel = new Model1
+        {
+            SomeInteger = 15
+        };
+
+        var pipeline = Pipeline
+            .Create()
+            .Process<Model1>(initModel)
+            .Add<Handler1, Model2>()
+            .Add<Handler2, Model3>()
+            .Add<Handler3, Model3>()
+            .SealWith<Handler4, Model4>();
+
+        var result = pipeline.Execute();
+    }
+}
