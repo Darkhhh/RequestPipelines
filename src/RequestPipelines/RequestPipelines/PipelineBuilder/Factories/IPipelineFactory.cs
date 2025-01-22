cf8f73cb73b18a8f01ebb3494d3e7ac2eeaac756
Darkhhh <a.a.mikov@mail.ru>
2025-01-22 13:23:04 +0300

    (dev): HANDP-10 - добавлена фабрика конвейеров.

---
+++ src/RequestPipelines/RequestPipelines/PipelineBuilder/Factories/IPipelineFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestPipelines.PipelineBuilder.Factories;
+
+/// <summary>
+/// Интерфейс фабрики конвейеров.
+/// </summary>
+public interface IPipelineFactory
+{
+    /// <summary>
+    /// Создать конвейер.
+    /// </summary>
+    /// <returns>Конвейер.</returns>
+    Pipeline CreatePipeline();
+}
