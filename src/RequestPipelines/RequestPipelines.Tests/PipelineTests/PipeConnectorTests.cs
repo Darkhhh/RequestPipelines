769dfce3d5f76a3646f2e16da7a7ee7206372b2d
Darkhhh <a.a.mikov@mail.ru>
2025-01-24 14:21:14 +0300

    (tests): HANDP-6 - обновлены тесты для PipeConnector.

--- src/RequestPipelines/RequestPipelines.Tests/PipelineTests/PipeConnectorTests.cs
+++ src/RequestPipelines/RequestPipelines.Tests/PipelineTests/PipeConnectorTests.cs
@@ -1,27 +1,80 @@
-﻿using RequestPipelines.Handlers;
+﻿using System.Linq.Expressions;
+using Moq;
+using RequestPipelines.Handlers;
 using RequestPipelines.PipelineBuilder;
+using RequestPipelines.Resolvers;
 
 namespace RequestPipelines.Tests.PipelineTests;
 
 [TestClass]
-public class PipeConnectorTests
+public class PipeConnectorTests : TestClassBase
 {
-    [TestMethod]
-    public void Add_WithInstances_Success()
+    private readonly Mock<Pipeline> _pipelineMock;
+    private readonly Mock<IHandlerResolver> _resolverMock;
+
+    public PipeConnectorTests()
     {
-        // _ = new PipeConnector<Model1>()
-        //     .Add(new Handler1())
-        //     .Add(new Handler2());
+        _resolverMock = GetMock<IHandlerResolver>();
+        _pipelineMock = new Mock<Pipeline>(_resolverMock.Object);
+        _pipelineMock.Object.Process(new Model1());
+    }
+    
+    [TestMethod]
+    public void Add_ReturnNextConnector_Success()
+    {
+        #region Arrange
+
+        var connector = Pipeline.Create(_resolverMock.Object).Process(new Model1());
+        _resolverMock.Setup(x => x.Resolve(typeof(Handler1))).Returns(new Handler1());
+
+        #endregion
+
+        #region Act
+
+        var nextConnector = connector.Add<Handler1, Model2>();
+
+        #endregion
+
+        #region Assert
+
+        Assert.IsInstanceOfType<PipeConnector<Model2>>(nextConnector);
+        
+        _resolverMock.Verify(
+            x => x.Resolve(It.IsAny<Type>()), 
+            Times.Once);
+
+        #endregion
     }
 
     [TestMethod]
-    public void Add_WithoutInstances_Success()
+    public void Add_ChainCalls_Success()
     {
-        _ = new PipeConnector<Model1>()
+        #region Arrange
+
+        var handlers = 2;
+        var connector = Pipeline.Create(_resolverMock.Object).Process(new Model1());
+        _resolverMock.Setup(x => x.Resolve(typeof(Handler1))).Returns(new Handler1());
+        _resolverMock.Setup(x => x.Resolve(typeof(Handler2))).Returns(new Handler2());
+
+        #endregion
+
+        #region Act
+
+        _ = connector
             .Add<Handler1, Model2>()
             .Add<Handler2, Model3>();
-    }
 
+        #endregion
+        
+        #region Assert
+        
+        _resolverMock.Verify(
+            x => x.Resolve(It.IsAny<Type>()), 
+            Times.Exactly(handlers));
+        
+        #endregion
+    }
+    
     private struct Model1;
     private struct Model2;
     private struct Model3;
