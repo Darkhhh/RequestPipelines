18e1b83e2a7a2b3dc9a41f2d1dcda900223fbc46
Darkhhh <a.a.mikov@mail.ru>
2025-01-27 12:44:58 +0300

    (tests): HANDP-6 - добавлены тесты для PipeConnector.

--- src/RequestPipelines/RequestPipelines.Tests/PipelineTests/PipeConnectorTests.cs
+++ src/RequestPipelines/RequestPipelines.Tests/PipelineTests/PipeConnectorTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using RequestPipelines.Handlers;
 using RequestPipelines.PipelineBuilder;
+using RequestPipelines.PipelineExecution;
 using RequestPipelines.Resolvers;
 
 namespace RequestPipelines.Tests.PipelineTests;
@@ -9,68 +10,141 @@ namespace RequestPipelines.Tests.PipelineTests;
 [TestClass]
 public class PipeConnectorTests : TestClassBase
 {
-    private readonly Mock<Pipeline> _pipelineMock;
-    private readonly Mock<IHandlerResolver> _resolverMock;
+    private readonly Mock<IHandlerResolver> _resolver;
+    private readonly Mock<IPipelineHelper> _helper;
 
     public PipeConnectorTests()
     {
-        _resolverMock = GetMock<IHandlerResolver>();
-        _pipelineMock = new Mock<Pipeline>(_resolverMock.Object);
-        _pipelineMock.Object.Process(new Model1());
+        _resolver = GetMock<IHandlerResolver>();
+        _helper = GetMock<IPipelineHelper>();
     }
-    
+
     [TestMethod]
-    public void Add_ReturnNextConnector_Success()
+    public void Add_OneHandler_Success()
     {
         #region Arrange
 
-        var connector = Pipeline.Create(_resolverMock.Object).Process(new Model1());
-        _resolverMock.Setup(x => x.Resolve(typeof(Handler1))).Returns(new Handler1());
+        var connector = new PipeConnector<Model1>(_helper.Object, _resolver.Object);
+        _resolver
+            .Setup(x => x.Resolve(It.IsAny<Type>()))
+            .Returns(new Handler1());
+        _helper
+            .Setup(x => x.GetCurrentInputParameter())
+            .Returns(Expression.Constant(new Model1()));
 
         #endregion
-
+        
         #region Act
 
         var nextConnector = connector.Add<Handler1, Model2>();
 
         #endregion
-
+        
         #region Assert
-
+        
         Assert.IsInstanceOfType<PipeConnector<Model2>>(nextConnector);
         
-        _resolverMock.Verify(
-            x => x.Resolve(It.IsAny<Type>()), 
+        _resolver.Verify(x => x.Resolve(It.IsAny<Type>()), 
             Times.Once);
+        _helper.Verify(x => x.AddExpression(It.IsAny<Expression>(), It.IsAny<ParameterExpression>()), 
+            Times.Once);
+        
+        #endregion
+    }
+    
+    [TestMethod]
+    public void SealWith_Success()
+    {
+        #region Arrange
 
+        var connector = new PipeConnector<Model1>(_helper.Object, _resolver.Object);
+        _resolver
+            .Setup(x => x.Resolve(It.IsAny<Type>()))
+            .Returns(new Handler1());
+        _helper
+            .Setup(x => x.GetCurrentInputParameter())
+            .Returns(Expression.Constant(new Model1()));
+        _helper
+            .Setup(x => x.AddFinalExpression<Model2>(It.IsAny<Expression>()))
+            .Returns(new PipelineExecutor<Model2>(() => new Model2()));
+
+        #endregion
+        
+        #region Act
+
+        var executor = connector.SealWith<Handler1, Model2>();
+
+        #endregion
+        
+        #region Assert
+        
+        Assert.IsTrue(executor.GetType().IsAssignableTo(typeof(IPipelineExecutor<Model2>)));
+        
+        _resolver.Verify(x => x.Resolve(It.IsAny<Type>()), 
+            Times.Once);
+        _helper.Verify(x => x.AddFinalExpression<Model2>(It.IsAny<Expression>()), 
+            Times.Once);
+        
         #endregion
     }
 
     [TestMethod]
-    public void Add_ChainCalls_Success()
+    public void AddActionT_Success()
     {
         #region Arrange
 
-        var handlers = 2;
-        var connector = Pipeline.Create(_resolverMock.Object).Process(new Model1());
-        _resolverMock.Setup(x => x.Resolve(typeof(Handler1))).Returns(new Handler1());
-        _resolverMock.Setup(x => x.Resolve(typeof(Handler2))).Returns(new Handler2());
+        var connector = new PipeConnector<Model1>(_helper.Object, _resolver.Object);
+        _helper
+            .Setup(x => x.GetCurrentInputParameter())
+            .Returns(Expression.Constant(new Model1()));
 
         #endregion
-
+        
         #region Act
 
-        _ = connector
-            .Add<Handler1, Model2>()
-            .Add<Handler2, Model3>();
+        var nextConnector = connector.AddAction(x => _ = x.GetType());
 
         #endregion
         
         #region Assert
         
-        _resolverMock.Verify(
-            x => x.Resolve(It.IsAny<Type>()), 
-            Times.Exactly(handlers));
+        Assert.IsInstanceOfType<PipeConnector<Model1>>(nextConnector);
+        
+        _resolver.Verify(x => x.Resolve(It.IsAny<Type>()), 
+            Times.Never);
+        _helper.Verify(x => x.GetCurrentInputParameter(), 
+            Times.Once);
+        _helper.Verify(x => x.AddExpression(It.IsAny<Expression>()), 
+            Times.Once);
+        
+        #endregion
+    }
+    
+    [TestMethod]
+    public void AddAction_Success()
+    {
+        #region Arrange
+
+        var connector = new PipeConnector<Model1>(_helper.Object, _resolver.Object);
+
+        #endregion
+        
+        #region Act
+
+        var nextConnector = connector.AddAction(() => Mock.Verify());
+
+        #endregion
+        
+        #region Assert
+        
+        Assert.IsInstanceOfType<PipeConnector<Model1>>(nextConnector);
+        
+        _resolver.Verify(x => x.Resolve(It.IsAny<Type>()), 
+            Times.Never);
+        _helper.Verify(x => x.GetCurrentInputParameter(), 
+            Times.Never);
+        _helper.Verify(x => x.AddExpression(It.IsAny<Expression>()), 
+            Times.Once);
         
         #endregion
     }
@@ -81,17 +155,11 @@ public class PipeConnectorTests : TestClassBase
     
     private class Handler1 : PipelineHandler<Model1, Model2>
     {
-        public override Model2 Handle(Model1 request)
-        {
-            return new Model2();
-        }
+        public override Model2 Handle(Model1 request) => new();
     }
     
     private class Handler2 : PipelineHandler<Model2, Model3>
     {
-        public override Model3 Handle(Model2 request)
-        {
-            return new Model3();
-        }
+        public override Model3 Handle(Model2 request) => new();
     }
 }
